4cb4e3ede4edfcc5b61065019b83fe004c71d77a
Dilshod <sayfiddinovd25@gmail.com>
2025-12-30 18:16:21 +0500

    Yangilandi

--- Javascript/config.js
+++ Javascript/config.js
@@ -1,27 +1,27 @@
 // Konfiguratsiya fayli
-        const API_KEY = '940702a307b0d1ca0eb2ae684c9384f8';
-        let unit = 'metric'; 
-        let lastCity = "Toshkent";
+const API_KEY = '940702a307b0d1ca0eb2ae684c9384f8';
+let unit = 'metric'; 
+let lastCity = "Toshkent";
 // Mavzu va ikonka xaritalari
-        const THEME_MAP = {
-            'Clear': 'bg-clear',
-            'Clouds': 'bg-clouds',
-            'Rain': 'bg-rain',
-            'Drizzle': 'bg-rain',
-            'Thunderstorm': 'bg-rain',
-            'Snow': 'bg-snow',
-            'Mist': 'bg-clouds',
-            'Smoke': 'bg-clouds',
-            'Haze': 'bg-clouds',
-            'Fog': 'bg-clouds'
-        };
+const THEME_MAP = {
+        'Clear': 'bg-clear',
+        'Clouds': 'bg-clouds',
+        'Rain': 'bg-rain',
+        'Drizzle': 'bg-rain',
+        'Thunderstorm': 'bg-rain',
+        'Snow': 'bg-snow',
+        'Mist': 'bg-clouds',
+        'Smoke': 'bg-clouds',
+        'Haze': 'bg-clouds',
+        'Fog': 'bg-clouds'
+};
 //  Ikonka xaritasi
-        const ICON_MAP = {
-            'Clear': 'fa-sun',
-            'Clouds': 'fa-cloud',
-            'Rain': 'fa-cloud-showers-heavy',
-            'Drizzle': 'fa-cloud-rain',
-            'Thunderstorm': 'fa-bolt',
-            'Snow': 'fa-snowflake',
-            'Atmosphere': 'fa-smog'
-        };
+const ICON_MAP = {
+        'Clear': 'fa-sun',
+        'Clouds': 'fa-cloud',
+        'Rain': 'fa-cloud-showers-heavy',
+        'Drizzle': 'fa-cloud-rain',
+        'Thunderstorm': 'fa-bolt',
+        'Snow': 'fa-snowflake',
+        'Atmosphere': 'fa-smog'
+};
